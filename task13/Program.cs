323868bfd21c31a4c29b803f819d83564b41fb4b
1MonkeySpy1 <sharondon2062@gmial.com>
2023-02-20 09:31:21 +0500

    Ready task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -8,3 +8,13 @@
 
 Console.WriteLine("Введите число");
 int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 100) {Console.WriteLine("Третьей цифры нет");}
+else 
+{
+    while (n > 999)
+     {
+        n = n  / 10; 
+     }
+    Console.WriteLine(n % 10); 
+}
